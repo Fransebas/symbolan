e7f862ba91cb470144479475b1312f985e0a3a9c
fransebas <ffransebas@gmail.com>
2020-08-23 16:27:09 -0500

    page updated

---
+++ webapp/build/precache-manifest.a9253e9ff047da95eb33443c28afbac2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb7545387789bb74a752e3c8a616310",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c0a733ec0d68e245d79",
+    "url": "/static/css/main.5a69ae26.chunk.css"
+  },
+  {
+    "revision": "7a471a8c1e9a8cdbbb29",
+    "url": "/static/js/2.ec219f7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ec219f7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c0a733ec0d68e245d79",
+    "url": "/static/js/main.807bddbd.chunk.js"
+  },
+  {
+    "revision": "b3cfd9f5cdf691e24e2f",
+    "url": "/static/js/runtime-main.00ed32f5.js"
+  }
+]);
